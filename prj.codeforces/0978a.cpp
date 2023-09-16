adbb990f865b598f0327d618b75712ef538b4328
masssssha <144623212+masssssha@users.noreply.github.com>
2023-09-16 15:14:12 +0300

    Create 0978a.cpp

---
+++ prj.codeforces/0978a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int t = 0;
+    std::vector <int> a;
+    std::cin >> t;
+    for (int i = 0; i < t; i++) {
+        int temp = 0;
+        std::cin >> temp;
+        a.push_back(temp);
+    }
+    std::vector <int> b;
+    int count = 0;
+    for (int i = a.size() - 1; i >= 1; i--) {
+        if (a[i] != -1000000) {
+            count += 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (a[i] == a[j]) {
+                    a[j] = -1000000;
+                }
+            }
+        }
+    }
+    if (a[0] != -1000000) {
+        count += 1;
+    }
+    std::cout << count << std::endl;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != -1000000) {
+            std::cout << a[i] << " ";
+        }
+    }
+}
