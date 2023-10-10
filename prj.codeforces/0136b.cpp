78957abd68b3b51f67c5ee8298d9562a8d390843
masssssha <144623212+masssssha@users.noreply.github.com>
2023-10-10 19:45:07 +0300

    Create 0136b.cpp

---
+++ prj.codeforces/0136b.cpp
@@ -0,0 +1,55 @@
+#include <iostream>   
+#include <vector>
+#include <cmath>
+
+int main() {
+    int a(0), c(0);
+    std::cin >> a >> c;
+    std::vector <int> ans;
+    int pupu = std::max(a, c);
+    while (pupu > 0) {
+        if (a % 3 == 0) {
+            if (c % 3 == 0) {
+                ans.push_back(0);
+            }
+            else if (c % 3 == 1) {
+                ans.push_back(1);
+            }
+            else {
+                ans.push_back(2);
+            }
+        }
+        else if (a % 3 == 1) {
+            if (c % 3 == 0) {
+                ans.push_back(2);
+            }
+            else if (c % 3 == 1) {
+                ans.push_back(0);
+            }
+            else {
+                ans.push_back(1);
+            }
+        }
+        else {
+            if (c % 3 == 0) {
+                ans.push_back(1);
+            }
+            else if (c % 3 == 1) {
+                ans.push_back(2);
+            }
+            else {
+                ans.push_back(0);
+            }
+        }
+        a /= 3;
+        c /= 3;
+        pupu /= 3;
+    }
+    int total(0);
+    int step = ans.size() - 1;
+    for (int i = ans.size() - 1; i >= 0; i--) {
+        total = total + ans[i] * pow(3, step);
+        step -= 1;
+    }
+    std::cout << total;
+}
