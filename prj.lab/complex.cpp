ed605319e0729043993e6901b050c8d4b1300a08
masssssha <144623212+masssssha@users.noreply.github.com>
2023-11-06 17:22:36 +0300

    Update complex.cpp

--- prj.lab/complex.cpp
+++ prj.lab/complex.cpp
@@ -98,28 +98,52 @@ void TestMul(Complex x, Complex y, double z) {
 
 void TestDiv(Complex x, Complex y, double z) {
     std::cout << x << " /= " << y << ": ";
-    x /= y;
-    std::cout << x << std::endl;
-    std::cout << x << " / " << y << " = " << (x / y) << std::endl;
-    std::cout << y << " / " << z << " = " << (y / z) << std::endl;
-    std::cout << z << " / " << x << " = " << (z / x) << std::endl;
+    if (y == Complex(0)) {
+        std::cout << "Error, denominator can't be zero" << std::endl;
+    }
+    else {
+        x /= y;
+        std::cout << x << std::endl;
+    }
+    std::cout << x << " / " << y << " = ";
+    if (y == Complex(0)) {
+        std::cout << "Error, denominator can't be zero" << std::endl;
+    }
+    else {
+        std::cout << (x / y) << std::endl;
+    }
+    std::cout << y << " / " << z << " = ";
+    if (z == 0) {
+        std::cout << "Error, denominator can't be zero" << std::endl;
+    }
+    else {
+        std::cout << (y / z) << std::endl;
+    }
+    std::cout << z << " / " << x << " = ";
+    if (x == Complex(0)) {
+        std::cout << "Error, denominator can't be zero" << std::endl;
+    }
+    else {
+        std::cout << (z / x) << std::endl;
+    }
 }
 
 int main()
 {
     Complex x(1, 8);
-    Complex y(2, 3);
-    double z = 2;
+    Complex y(0, 0);
+    double z = 2.0;
     std::cout << "Addition test: " << std::endl;
     TestAdd(x, y, z);
-    z = 5;
+    z = 5.0;
     std::cout << std::endl;
     std::cout << "Subtraction test: " << std::endl;
     TestSub(x, y, z);
-    z = -3;
+    z = -3.0;
     std::cout << std::endl;
     std::cout << "Multiplication test: " << std::endl;
     TestMul(x, y, z);
+    z = 1.0;
     std::cout << std::endl;
     std::cout << "Division test: " << std::endl;
     TestDiv(x, y, z);
