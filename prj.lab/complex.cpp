cdc9b5d9ae2c23ed8a93afb5465aa44da3a9e091
masssssha <144623212+masssssha@users.noreply.github.com>
2023-11-06 16:57:08 +0300

    Update complex.cpp

--- prj.lab/complex.cpp
+++ prj.lab/complex.cpp
@@ -25,47 +25,104 @@ inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) {
     return rhs.writeTo(ostrm);
 }
 
+//Сложение двух комплексных, комплексное + вещественное
 Complex operator+(const Complex& x, const Complex& y) {
     return Complex(x) += y;
 }
-Complex operator-(const Complex& lhs, const Complex& rhs) {
-    return Complex(lhs) -= rhs;
+Complex operator+(const Complex& x, const double& y) {
+    return Complex(x) += Complex(y);
 }
+Complex operator+(const double& x, const Complex& y) {
+    return Complex(x) += Complex(y);
+}
+
+//Вычитание двух комплексных, комплексное - вещественное
+Complex operator-(const Complex& x, const Complex& y) {
+    return Complex(x) -= y;
+}
+Complex operator-(const Complex& x, const double& y) {
+    return Complex(x) -= Complex(y);
+}
+Complex operator-(const double& x, const Complex& y) {
+    return Complex(x) -= Complex(y);
+}
+
+//Умножение двух комплексных, комплексное * вещественное
 Complex operator*(const Complex& x, const Complex& y) {
     return Complex(x) *= y;
 }
+Complex operator*(const Complex& x, const double& y) {
+    return Complex(x) *= Complex(y);
+}
+Complex operator*(const double& x, const Complex& y) {
+    return Complex(x) *= Complex(y);
+}
+
+//Деление двух комплексных, комплексное / вещественное
 Complex operator/(const Complex& x, const Complex& y) {
     return Complex(x) /= y;
+}
+Complex operator/(const Complex& x, const double& y) {
+    return Complex(x) /= Complex(y);
+}
+Complex operator/(const double& x, const Complex& y) {
+    return Complex(x) /= Complex(y);
+}
+
+void TestAdd(Complex x, Complex y, double z) {
+    std::cout << x << " += " << y << ": ";
+    x += y;
+    std::cout << x << std::endl;
+    std::cout << x << " + " << y << " = " << (x + y) << std::endl;
+    std::cout << y << " + " << z << " = " << (y + z) << std::endl;
+    std::cout << z << " + " << x << " = " << (z + x) << std::endl;
+}
+
+void TestSub(Complex x, Complex y, double z) {
+    std::cout << x << " -= " << y << ": ";
+    x -= y;
+    std::cout << x << std::endl;
+    std::cout << x << " - " << y << " = " << (x - y) << std::endl;
+    std::cout << y << " - " << z << " = " << (y - z) << std::endl;
+    std::cout << z << " - " << x << " = " << (z - x) << std::endl;
+}
+
+void TestMul(Complex x, Complex y, double z) {
+    std::cout << x << " *= " << y << ": ";
+    x *= y;
+    std::cout << x << std::endl;
+    std::cout << x << " * " << y << " = " << (x * y) << std::endl;
+    std::cout << y << " * " << z << " = " << (y * z) << std::endl;
+    std::cout << z << " * " << x << " = " << (z * x) << std::endl;
+}
+
+void TestDiv(Complex x, Complex y, double z) {
+    std::cout << x << " /= " << y << ": ";
+    x /= y;
+    std::cout << x << std::endl;
+    std::cout << x << " / " << y << " = " << (x / y) << std::endl;
+    std::cout << y << " / " << z << " = " << (y / z) << std::endl;
+    std::cout << z << " / " << x << " = " << (z / x) << std::endl;
 }
 
 int main()
 {
-    Complex z(1, 8);
+    Complex x(1, 8);
     Complex y(2, 3);
-    std::cout << "First complex number: " << z << std::endl;
-    std::cout << "Second complex number: " << y << std::endl;
-    Complex w = z;
-    std::cout << "Third complex number = first: " << w << " = " << z << std::endl;
-    std::cout << z << " += " << w << ": ";
-    z += w;
-    std::cout << z << std::endl;
-    std::cout << y << " -= " << z << ": ";
-    y -= z;
-    std::cout << y << std::endl;
-    std::cout << w << " *= " << z << ": ";
-    w *= z;
-    std::cout << w << std::endl;
-    std::cout << w << " /= " << z << ": ";
-    w /= z;
-    std::cout << z << std::endl;
-    std::cout << Complex(8, 0) << " + " << y << " = " << (Complex(8, 0) + y) << std::endl;
-    std::cout << Complex(8, 0) << " - " << w << " = " << (Complex(8, 0) - w) << std::endl;
-    std::cout << Complex(8, 0) << " * " << y << " = " << (Complex(8, 0) * y) << std::endl;
-    std::cout << Complex(8, 0) << " / " << Complex(1, 0) << " = " << (Complex(8, 0) / Complex(1, 0)) << std::endl;
-    std::cout << z << " + " << 3 << " = " << (z + Complex(3)) << std::endl;
-    std::cout << y << " - " << 2.1 << " = " << (y - Complex(2.1)) << std::endl;
-    std::cout << w << " * " << 4 << " = " << (w * Complex(4)) << std::endl;
-    std::cout << z << " / " << 5 << " = " << (z / Complex(5)) << std::endl;
+    double z = 2;
+    std::cout << "Addition test: " << std::endl;
+    TestAdd(x, y, z);
+    z = 5;
+    std::cout << std::endl;
+    std::cout << "Subtraction test: " << std::endl;
+    TestSub(x, y, z);
+    z = -3;
+    std::cout << std::endl;
+    std::cout << "Multiplication test: " << std::endl;
+    TestMul(x, y, z);
+    std::cout << std::endl;
+    std::cout << "Division test: " << std::endl;
+    TestDiv(x, y, z);
 }
 
 Complex::Complex(const double real, const double imaginary) : re(real), im(imaginary) {}
